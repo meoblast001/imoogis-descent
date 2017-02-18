d2960327fdc27d38a6eeb3b8928f232be770119a
Braden Walters <vc@braden-walters.info>
2017-02-18 23:12:56 +0100

    Lock vertically to the character's vertical sea level; Follow character more closely in level transition regions

--- game/VerticalLevelCamera.ts
+++ game/VerticalLevelCamera.ts
@@ -2,6 +2,7 @@
 /// <reference path="Resources.ts" />
 
 class VerticalLevelCamera extends ex.BaseCamera {
+  public static readonly TransitionRegion = 20;
   private xOffset;
   private yOffset;
 
@@ -12,11 +13,27 @@ class VerticalLevelCamera extends ex.BaseCamera {
 
   public getFocus(): ex.Vector {
     if (this._follow) {
+      // On the X axis, the player character is followed.
+      var focusX = this._follow.pos.x + this._follow.getWidth() / 2
+      // On the Y axis, the camera is in the player character's vertical level.
       var focusY = Math.floor(this._follow.pos.y / GameSize.Height) *
                    GameSize.Height + GameSize.Height / 2;
-      return new ex.Vector(
-        this._follow.pos.x + this._follow.getWidth() / 2 + this.xOffset,
-        focusY + this.yOffset);
+      var playerYInLevel = this._follow.pos.y % GameSize.Height;
+      // Follow closely if the player is in the transition regions.
+      if (playerYInLevel < VerticalLevelCamera.TransitionRegion) {
+        var adjustY = playerYInLevel - VerticalLevelCamera.TransitionRegion;
+        return new ex.Vector(focusX + this.xOffset,
+                             focusY + this.yOffset + adjustY);
+      } else if (playerYInLevel > GameSize.Height -
+                                  VerticalLevelCamera.TransitionRegion) {
+        var adjustY = playerYInLevel -
+                      (GameSize.Height - VerticalLevelCamera.TransitionRegion);
+        return new ex.Vector(focusX + this.xOffset,
+                             focusY + this.yOffset + adjustY);
+      // End of transition region code.
+      } else {
+        return new ex.Vector(focusX + this.xOffset, focusY + this.yOffset);
+      }
     } else {
       super.getFocus();
     }
